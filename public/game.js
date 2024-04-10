b202155f1149899bd12ecac7de5c71fb0898bbad
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2024-04-11 00:27:31 +0200

    Update game duration to 257 seconds

--- public/game.js
+++ public/game.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let currentWordIndex = 0;
   let correctWordsCount = getTodayCorrectWordsCount(); // Get today's correct words count if available
   let countdown;
-  let gameDuration = 10;
+  let gameDuration = 257;
   const today = new Date();
   let gameHasEndedToday = false;
 
