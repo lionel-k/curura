e6090634039282f33a6b838d4811253c79fc898f
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2024-04-10 17:20:25 +0200

    Add leaderboard endpoint to serve leaderboard template

--- app.rb
+++ app.rb
@@ -13,7 +13,12 @@ get '/' do
   erb :index
 end
 
-# Endpoint to serve the words CSV file
 get '/words' do
   send_file 'public/words.csv', type: :csv
+end
+
+get '/leaderboard' do
+  # You will also fetch and pass leaderboard data to the template
+  @leaderboard = []
+  erb :leaderboard
 end
